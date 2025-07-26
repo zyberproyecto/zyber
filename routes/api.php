a78c87bf822d5bf1048c9c929711297eab918603
ianmiller3322@gmail.com <ianmiller3322@gmail.com>
2025-07-26 09:34:33 -0300

    Creacion de la logica del login en el API de usuarios, y conexion con la landing page

--- routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-use App\Http\Controllers\UsuarioController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('http://localhost:8000/api/usuarios/login', [UsuarioController::class, 'Logear']);
