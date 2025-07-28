fdb022401075ee0a0dfda7c59b4ca2a4f3f027f1
ianmiller3322@gmail.com <ianmiller3322@gmail.com>
2025-07-28 13:57:44 -0300

    Creacion de la logica del registro de usuarios y migration de las tablas faltantes de la base de datos

--- app/Models/usuarios.php
+++ app/Models/usuarios.php
@@ -4,39 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class usuarios extends Authenticatable
 {
-    // Nombre de la tabla
-    protected $table = 'usuarios';
+	protected $table = 'usuarios';
+	protected $primaryKey = 'ci_usuario';
+	public $incrementing = false;
+	protected $keyType = 'string';
+	protected $fillable = [
+		'ci_usuario',
+		'primer_nombre',
+		'segundo_nombre',
+		'primer_apellido',
+		'segundo_apellido',
+		'contraseña',
+		'estado_registro',
+	];
+	protected $hidden = [
+		'contraseña',
+	];
+	public $timestamps = true;
+	public function getAuthPassword()
+	{
+		return $this->contraseña;
+	}
 
-    // Clave primaria personalizada
-    protected $primaryKey = 'ci_usuario';
-    public $incrementing = false;
-    protected $keyType = 'string';
-
-    // Campos que pueden ser asignados masivamente
-    protected $fillable = [
-        'ci_usuario',
-        'primer_nombre',
-        'segundo_nombre',
-        'primer_apellido',
-        'segundo_apellido',
-        'contraseña',
-        'estado_registro',
-    ];
-
-    // Si quieres ocultar la contraseña en respuestas JSON
-    protected $hidden = [
-        'contraseña',
-    ];
-
-    // Si usas timestamps
-    public $timestamps = true;
-
-    // Si tu campo de contraseña se llama diferente a "password"
-    public function getAuthPassword()
-    {
-        return $this->contraseña;
-    }
+	use SoftDeletes;
 }
+
